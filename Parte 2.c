d9072306b857ce5942ea37db298a09cac68b4c9e
Kustonm <65606861+Kustonm@users.noreply.github.com>
2020-05-24 21:14:12 +0200

    Update Parte 2.c

--- Parte 2.c
+++ Parte 2.c
@@ -13,16 +13,24 @@
 int main (int argc , char *argv[]){
 
   DIR *d;
-  
   struct dirent *dir;
-  
-  if (d == NULL){
-    printf("No se a podido abrir");
+  dir= opendir(argv[1]);
+  char *path;
+  sprintf(path, "%s/%s", argv[1], dir->d_name);    
+   
+    if (d == NULL){
+    printf("No se ha podido abrir");
     return (0);
   }
   
-  while((dir == readdir(d) != NULL){
-      if 
+  while((dir == readdir(d)) != NULL){
+       if (dir->d_type == DT_DIR){
+          chmod( path, 0550);//Pello mira teteas (. Y .) 3==D ahahah mirarrona
+       }else{
+          if(dir->d_type == DT_REG){
+              chmod(path, 00022 );
+          }
+       }      
   }
       
  closedir(d);
