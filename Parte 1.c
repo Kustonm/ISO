b1c618c2142ce4ac3c42bbd506ea943778448d90
Kustonm <65606861+Kustonm@users.noreply.github.com>
2020-05-24 22:29:29 +0200

    Update Parte 1.c

--- Parte 1.c
+++ Parte 1.c
@@ -1,78 +1,107 @@
 #include <stdio.h>
-#include <stdbool.h>
-#include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
 #include <dirent.h>
-#include <fcntl.h>
+#include <errno.h>
+#include <ctype.h>
 
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>
 
-#include <errno.h>
 
-int main (int argc , char *argv[]){
+#define MAX_BUF 512
 
-DIR *d;
-int fich = open(argv[2], O_WRONLY|O_APPEND|O_CREAT, 0600);
-int Pos1;
-int Pos2;
+int main(int argc, char** argv){
+	if (argc != 5){
+		fprintf(stderr, "Se esperaban 4 argumentos pero se recivieron %d\n", argc);
+		fprintf(stderr, "Modo de empleo: \"./util_1 dir1 resultado  pos1 pos2\"\n");
+		exit(-1);
+	}
 
-char error[512];
-struct dirent *dir;
-char aux[512];
-char path[512];
-d = opendir(argv[1]);
-dir =readdir(d);
+	char buf1[MAX_BUF];
+	char buf2[MAX_BUF*2];
 
-while((dir != NULL)){
-	if ((dir->d_type & DT_LNK == 0) && (dir->d_type & DT_REG != 0) ){
-		sprintf(path,"./%s/%s", argv[1], dir->d_name);
+	DIR *dir_stream = opendir(argv[1]);
+	if (!dir_stream){
+		sprintf(buf1,"Error al abrir el directorio \"%s\": ", argv[1]);
+		perror(buf1);
+		exit(-2);
+	}	
+
+	int result_fd = open(argv[2], O_WRONLY | O_CREAT, 0664);
+	if (!result_fd){
+		sprintf(buf1,"Error al crear y abrir el fichero \"%s\":", argv[2]);
+		perror(buf1);
+		exit(-3);
+	}
+
+	int i;
+	int strlen_1 = strlen(argv[3]);
+	for (i = 0 ; i < strlen_1 ; i++){
+		if (!isdigit(argv[3][i])){
+			fprintf(stderr,"El argumento 3 \"pos1\" ha de ser un numero.\n");
+			exit(-4);
 		}
-		int auxF  = open(path, O_RDONLY);
-		
-		if (auxF == -1){
-			sprintf(error, "No se puede abiri el fichero\n",dir->d_name); 
-			perror(error);
+	}
+	int pos1 = atoi(argv[3]);
+	int strlen_2 = strlen(argv[4]);
+	for (i = 0 ; i < strlen_2 ; i++){
+		if (!isdigit(argv[4][i])){
+			fprintf(stderr,"El argumento 4 \"pos2\" ha de ser un numero.\n");
+			exit(-4);
 		}
+	}	
+	int pos2 = atoi(argv[4]);
+
+	if (pos1 >= pos2){
+		fprintf(stderr, "Error: el argumento pos1 (%d) es mayor que pos2 (%d)\n", pos1, pos2);
+		exit(-5);
+	}
+
+
+	struct dirent *dir;
+	while(dir = readdir(dir_stream)){
+		if ((dir->d_type & (DT_LNK | DT_REG)) != DT_REG)
+			continue;
+
+		sprintf(buf1, "%s/%s", argv[1], dir->d_name);
+		int fd = open(buf1, O_RDONLY);
+		if (!fd){
+			sprintf(buf2, "Error al abrir el fichero \"%s\":", buf1);
+			perror(buf2);
+			continue;
+		}
+
+		int len = lseek(fd, 0, SEEK_END);
+		if (pos1 > len){
+			fprintf(stderr, "pos1 (%d) es mayor que la longitud del fichero %s (%d)\n", pos1, buf1, len);
+			continue;
+		}
+		if (pos2 > len){
+			fprintf(stderr, "pos2 (%d) es mayor que la longitud del fichero %s (%d)\n", pos2, buf1, len);
+			continue;
+		}
+
+		int pos = lseek(fd, pos1, SEEK_SET);
+		while (pos < pos2){
+			int read_len = pos2 - pos;
+			if (read_len > MAX_BUF) 
+				read_len = MAX_BUF;
+
+			read(fd, buf2, read_len);
+			write(result_fd, buf2, read_len);
+
+			pos += read_len;
+		}
+
+		close(fd);
+
+	}
+	closedir(dir_stream);
+	close(result_fd);
 	
-		int lenght = lseek(auxF, 0, SEEK_END);
-		
-		if(lenght < Pos1){
-			sprintf(error, "Fuera del limite %d.\n",dir->d_name);
-			perror(error);
-		}
-		 if (lenght < Pos2){
-		 	if ((lenght - Pos1) > 512){
-				while(lenght - Pos1  > 512){
-					lseek(auxF,Pos1,SEEK_SET);
-					read(auxF,aux,512);
-					write(fich,aux,512);
-				}
-				lseek(auxF,Pos1,SEEK_SET);
-				read(auxF,aux,(lenght - Pos1));
-				write(fich,aux,(lenght- Pos1));
-		}else{
-			if ((Pos2 - Pos1) > 512 ){
-				while( Pos2 - Pos1 > 512){
-					lseek(auxF, Pos1 ,SEEK_SET);
-					read(auxF, aux, 512);
-					write(fich,aux,512);
-				}
-				lseek(auxF,Pos1,SEEK_SET);
-				read(auxF,aux,(Pos2 -Pos1));
-				write(fich,aux,(Pos2 -Pos1));
-		      }else{
-		      		lseek(auxF,Pos1,SEEK_SET);
-				read(auxF,aux,Pos2);
-				write(fich,aux,Pos2);
-	}
-	}
+	exit(0);
 
 }
-
-closedir(d);
-close(fich);
-return(0);
-}
-}
-
