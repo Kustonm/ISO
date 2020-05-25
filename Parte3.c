5d96a3c6cb3fffba0f5d91a93d3036fb3bf6c45f
Kustonm <65606861+Kustonm@users.noreply.github.com>
2020-05-25 19:11:04 +0200

    Update Parte3.c

--- Parte3.c
+++ Parte3.c
@@ -12,8 +12,29 @@
 
 int main (int argc , char *argv[]){
 
-  pid_t 
-  int prog = system(argv[1]);
+  pid_t pid;
+  int my_pipe[2];
+  int ret;
+  char BUFFER_1[512];  
+  
+  int prog1 = system(argv[1]);
+  
+  int prog2 = system(argv[1]);
   int s_fd = open(argv[3], O_WRONLY | O_CREAT, 0664);
+    
+  if (ret == -1 ){
+      perror("pipe");
+      exit(1);
+  }
+  
+  
+  close(my_pipe[1]);
+  dup(my_pipe[0],STDOUT_FILENO);
+  close(my_pipe[0]);
+  
+  if(pid == 0){     
+    
+  
 
+   
 }
