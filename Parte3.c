9f1af8f14beb40083354b8f7bef380323c6e02f0
Kustonm <65606861+Kustonm@users.noreply.github.com>
2020-05-25 19:34:30 +0200

    Update Parte3.c

--- Parte3.c
+++ Parte3.c
@@ -11,30 +11,27 @@
 #include <errno.h>
 
 int main (int argc , char *argv[]){
-
-  pid_t pid;
-  int my_pipe[2];
-  int ret;
-  char BUFFER_1[512];  
   
-  int prog1 = system(argv[1]);
+  char buf1[512];
+  char buf2[512];
   
-  int prog2 = system(argv[1]);
   int s_fd = open(argv[3], O_WRONLY | O_CREAT, 0664);
     
-  if (ret == -1 ){
-      perror("pipe");
-      exit(1);
+  pid_t pid;
+  int pipe[2];
+  
+  dup2(pipe[1],1);
+    int prog1 = system(argv[1]);
+    read(1, buf1, 512);
+    int prog2 = system(argv[1]);
+    reda(1,buf2,512);
+  
+  if(strcmp(buf1,buf2) == 0 ){
+      write(s_fd, buf1,512);
   }
   
-  
-  close(my_pipe[1]);
-  dup(my_pipe[0],STDOUT_FILENO);
-  close(my_pipe[0]);
-  
-  if(pid == 0){     
-    
-  
-
-   
+  close(s_fd);
+  close(pipe[1]);
+  close(pipe[0]);
+  return(0);
 }
