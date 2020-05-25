cf6e07412b6d2e1e054711dd3e0e54f3d7a95c38
Kustonm <65606861+Kustonm@users.noreply.github.com>
2020-05-25 15:10:12 +0200

    Create Parte3.c

---
+++ Parte3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <string.h>
+#include <dirent.h>
+#include <fcntl.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+int main (int argc , char *argv[]){
+
+
+}
