e64f9c7591e5b7708a06bd3a697f806de98d1a69
Kustonm <65606861+Kustonm@users.noreply.github.com>
2020-05-25 11:19:30 +0200

    Update Parte 2.c

--- Parte 2.c
+++ Parte 2.c
@@ -7,29 +7,28 @@
 
 #include <sys/types.h>
 #include <sys/stat.h>
-
 #include <errno.h>
 
 int main (int argc , char *argv[]){
 
   DIR *d;
-  struct dirent *dir;
-  dir= opendir(argv[1]);
-  char *path;
-  sprintf(path, "%s/%s", argv[1], dir->d_name);    
+  d=opendir(argv[1]);
+  char *path;    
    
-    if (d == NULL){
-    printf("No se ha podido abrir");
-    return (0);
+  if (d == NULL){
+        printf("No se ha podido abrir");
+        return (0);
   }
+
+  //el estilo gangnam es sin duda mi estilo preferido. dab.
+  sprintf(path, "%s/%s", argv[1], argv[1]);
+  struct dirent *dir;
   
-  while((dir == readdir(d)) != NULL){
+  while(dir=readdir(d)){
        if (dir->d_type == DT_DIR){
           chmod( path, 0550);//Pello mira teteas (. Y .) 3==D ahahah mirarrona
-       }else{
-          if(dir->d_type == DT_REG){
-              chmod(path, 00022 );
-          }
+       }else if(dir->d_type == DT_REG){
+          chmod(path, 0022 );
        }      
   }
       
