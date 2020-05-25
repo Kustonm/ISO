852e79018f21bfd6db2d3f82612acc11fb0eae3b
Kustonm <65606861+Kustonm@users.noreply.github.com>
2020-05-25 22:47:01 +0200

    Update Parte4.c

--- Parte4.c
+++ Parte4.c
@@ -24,17 +24,16 @@ int main() {
           printf("Error al cerrar el canal 1.\n");
         }
         if(dup(p1[1]) == -1){
-          printf("Error dup.\n");
-          
+          printf("Error dup.\n");  
         }
         if((close(p1[1])) == -1){ 
-            printf("Error al cerrar p1[1].\n");
+            printf("Error al cerrar Pipe 1.\n");
         }
         if((close(p1[0])) == -1){
-          printf("Error al cerrar p1[0].\n");
+          printf("Error al cerrar Pipe 1.\n");
         }
         if((close(p2[0])) == -1){
-          printf("Error al cerrar p2[0].\n");
+          printf("Error al cerrar Pipe 2.\n");
         }
         if((close(p2[1])) == -1){
           printf("Error al cerrar p2[1].\n");
@@ -43,74 +42,76 @@ int main() {
         execlp("who", "who", NULL);
 
     } else if(pid == -1) {
-    printf("No ha sido posible crear el primer hijo.\n");
+    printf("No se a podiddo generear el primer subproceso \n");
     }
   
     if((pid == 0) {
-        if(close(0) == -1){ printf ("Error al cerrar el canal 0.\n"); 
+        if(close(0) == -1){ 
+            printf ("Error al cerrar el canal 0.\n"); 
         }
-
-        if(dup(p1[0]) == -1){ printf("Error dup.\n"); 
+        if((close(p1[0])) == -1){ 
+            printf("Error al cerrar Pipe 1.\n");
         }
-
-        if((close(p1[0])) == -1){ printf("Error al cerrar p1[0].\n");
-        }
-
-        if((close(p1[1])) == -1){ printf("Error al cerrar p1[1].\n");
+        if((close(p1[1])) == -1){ 
+            printf("Error al cerrar Pipe1.\n");
         }
 
         if(close(1) == -1){ printf("Error al cerrar el canal 1.\n");
         }
 
-        if(dup(p2[1]) == -1){ printf("Error dup.\n");}
-
-        if((close(p2[1])) == -1){ printf("Error al cerrar p2[1].\n");
+        if((close(p2[1])) == -1){ 
+            printf("Error al cerrar Pipe2.\n");
         }
 
-        if((close(p2[0])) == -1){ printf("Error al cerrar p2[0].\n");
+        if((close(p2[0])) == -1){ 
+            printf("Error al cerrar Pipe2.\n");
         }
 
         execlp("grep", "grep", "acaf00", NULL);
 
-    } else{ if(pid == -1){ printf("No ha sido posible crear el segundo hijo.\n");}
+    } else{ if(pid == -1){ 
+        printf("No se a podiddo generear el segundo subproceso \n".\n");}
 
     if((pid = fork()) == 0){
-        if(close(0) == -1){ printf("Error al cerrar el canal 0.\n");}
-
-        if(dup(p2[0]) == -1){ error("Error dup.\n");}
-
-        if((close(p2[0])) == -1){ printf("Error al cerrar p2[0].\n");
+        if(close(0) == -1){ 
+            printf("Error al cerrar el canal 0.\n");
+        }
+        if((close(p2[0])) == -1){ 
+            printf("Error al cerrar Pipe2.\n");
+        }
+        if((close(p2[1])) == -1){ 
+            printf("Error al cerrar Pipe2.\n");
+        }
+        if((close(p1[0])) == -1){ 
+            printf("Error al cerrar Pipe1.\n");
         }
 
-        if((close(p2[1])) == -1){ printf("Error al cerrar p2[1].\n");
-        }
-
-        if((close(p1[0])) == -1){ printf("Error al cerrar p1[0].\n");
-        }
-
-        if((close(p1[1])) == -1){ printf("Error al cerrar p1[1].\n");
+        if((close(p1[1])) == -1){ 
+            printf("Error al cerrar p1[1].\n");
         }
 
         execlp("sort", "sort", NULL);
         
 
-    } else if(pid == -1){ error("No ha sido posible crear el tercer hijo.\n");
+    } else if(pid == -1){ 
+        printf("No se a podiddo generear el tercer subproceso \n".\n");
     }
-
     wait(NULL);
-
-    if((close(p1[0])) == -1){ printf("Error al cerrar p1[0].\n");
+    if((close(p1[0])) == -1){ 
+        printf("Error al cerrar Pipe1.\n");
     }
 
-    if((close(p1[1])) == -1){ printf("Error al cerrar p1[1].\n");
+    if((close(p1[1])) == -1){
+        printf("Error al cerrar Pipe1.\n");
     }
    
-    if((close(p2[0])) == -1){ printf("Error al cerrar p2[0].\n");
+    if((close(p2[0])) == -1){ 
+        printf("Error al cerrar Pipe2.\n");
     }
 
-    if((close(p2[1])) == -1){ printf("Error al cerrar p2[1].\n");
+    if((close(p2[1])) == -1){ 
+        printf("Error al cerrar Pipe2.\n");
     }
-
     return 0;
 }
 }
